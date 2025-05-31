1392b61fa21286ce6529fd2446403d27687c9ccd
emancipat3r <ande2541@gmail.com>
2025-05-30 22:42:19 -0400

    update

--- utils/printer.go
+++ utils/printer.go
@@ -1,16 +1,42 @@
 package utils
 
-import {
+import (
 	"fmt"
+	"time"
+
 	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	//timestampStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
+	timestampStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("247"))
+	bracketStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("7")).Bold(true)  // White bold
+	infoStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("39")).Bold(true) // Blue
+	debugStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("247")).Italic(true)
+	errorStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("196")).Bold(true)
+	warnStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("214")).Bold(true)
+)
+
+func timestamp() string {
+	return timestampStyle.Render(time.Now().Format("15:04:05"))
 }
 
-// global style
-var style = lipgloss.NewStyle(),
-    Foreground(lipgloss.Color("205")),
-    Padding(0, 1),
-    Bold(true)
+func bracketed(label string, style lipgloss.Style) string {
+	return bracketStyle.Render("[") + style.Render(label) + bracketStyle.Render("]")
+}
 
-func LgPrint(msg string) {
-fmt.Println(style.Render(msg))
+func Info(msg string) {
+	fmt.Printf("%s %s  %s\n", timestamp(), bracketed("INFO", infoStyle), msg)
+}
+
+func Debug(msg string) {
+	fmt.Printf("%s %s %s\n", timestamp(), bracketed("DEBUG", debugStyle), msg)
+}
+
+func Error(msg string) {
+	fmt.Printf("%s %s %s\n", timestamp(), bracketed("ERROR", errorStyle), msg)
+}
+
+func Warn(msg string) {
+	fmt.Printf("%s %s  %s\n", timestamp(), bracketed("WARN", warnStyle), msg)
 }
