f99c7f276d6a4374d8dbc750afd57fe1e1ab5197
emancipat3r <23742165+emancipat3r@users.noreply.github.com>
2025-06-09 22:26:17 -0400

    add json prettyprinting and function to list linodes

--- utils/linode.go
+++ utils/linode.go
@@ -18,33 +18,64 @@ func GetLinodeAPIKey(configFile string, provider string) string {
 	return providerKey
 }
 
-func GetLinodeAccount(providerKey string) error {
+func GetLinodeAccount(providerKey string) (string, error) {
 	if providerKey == "" {
-		return logger.Errorf("Provider key is empty")
+		return "", logger.Errorf("Provider key is empty")
 	}
 
 	request, err := http.NewRequest("GET", "https://api.linode.com/v4/account", nil)
 	if err != nil {
-		return err
+		return "", err
 	}
 	request.Header.Set("Authorization", "Bearer "+providerKey)
 
 	client := &http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer response.Body.Close()
 
 	responseBytes, err := io.ReadAll(response.Body)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	if response.StatusCode != http.StatusOK {
-		return logger.Errorf("Unexpected status code: %s | %s", strconv.Itoa(response.StatusCode), string(responseBytes))
+		return "", logger.Errorf("Unexpected status code: %s | %s", strconv.Itoa(response.StatusCode), string(responseBytes))
 	}
 
-	logger.Info(string(responseBytes))
-	return nil
+	formattedResponse := PrettyPrintJSON(responseBytes)
+	return formattedResponse, nil
+}
+
+func ListLinodes(providerKey string) (string, error) {
+	if providerKey == "" {
+		return "", logger.Errorf("Provider key is empty")
+	}
+
+	request, err := http.NewRequest("GET", "https://api.linode.com/v4/linode/instances?page=1&page_size=100", nil)
+	if err != nil {
+		return "", err
+	}
+	request.Header.Set("Authorization", "Bearer "+providerKey)
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	responseBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return "", logger.Errorf("Unexpected status code: %s | %s", strconv.Itoa(response.StatusCode), string(responseBytes))
+	}
+
+	formattedResponse := PrettyPrintJSON(responseBytes)
+	return formattedResponse, nil
 }
