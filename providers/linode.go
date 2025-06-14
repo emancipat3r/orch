cc07697c9ba8526a2e1ee8d4f05a79fbd33351ca
emancipat3r <ande2541@gmail.com>
2025-06-13 22:40:58 -0400

    create more linode funcs

--- providers/linode.go
+++ providers/linode.go
@@ -1,16 +1,19 @@
 package providers
 
 import (
+	"bytes"
 	"io"
 	"net/http"
 	"strconv"
 
 	"github.com/emancipat3r/vps3/logger"
+
+	"github.com/emancipat3r/vps3/utils"
 )
 
 func GetLinodeAPIKey(configFile string, provider string) string {
 
-	providerKey := ParseCreds(configFile, provider)
+	providerKey := utils.ParseCreds(configFile, provider)
 	if providerKey == "" {
 		logger.Error("Failed to parse provider credentials")
 		return ""
@@ -45,7 +48,7 @@ func GetLinodeAccount(providerKey string) (string, error) {
 		return "", logger.Errorf("Unexpected status code: %s | %s", strconv.Itoa(response.StatusCode), string(responseBytes))
 	}
 
-	formattedResponse := PrettyPrintJSON(responseBytes)
+	formattedResponse := utils.PrettyPrintJSON(responseBytes)
 	return formattedResponse, nil
 }
 
@@ -76,6 +79,73 @@ func ListLinodes(providerKey string) (string, error) {
 		return "", logger.Errorf("Unexpected status code: %s | %s", strconv.Itoa(response.StatusCode), string(responseBytes))
 	}
 
-	formattedResponse := PrettyPrintJSON(responseBytes)
+	formattedResponse := utils.PrettyPrintJSON(responseBytes)
+	return formattedResponse, nil
+}
+
+func ListLinodeRegions(providerKey string) (string, error) {
+	if providerKey == "" {
+		return "", logger.Errorf("Provider key is empty")
+	}
+
+	request, err := http.NewRequest("GET", "https://api.linode.com/v4/regions", nil)
+	if err != nil {
+		return "", err
+	}
+	request.Header.Set("Accept", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	responseBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return "", logger.Errorf("Unexpected status code: %s | %s", strconv.Itoa(response.StatusCode), string(responseBytes))
+	}
+
+	formattedResponse := utils.PrettyPrintJSON(responseBytes)
+	return formattedResponse, nil
+}
+
+func CreateLinode(providerKey string) (string, error) {
+	if providerKey == "" {
+		return "", logger.Errorf("Provider key is empty")
+	}
+
+	jsonBody := []byte(`{
+ 		"booted": true,
+ 		"swap_size": 512
+	}`)
+	request, err := http.NewRequest("POST", "https://api.linode.com/v4/linode/instances", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return "", err
+	}
+	request.Header.Set("Authorization", "Bearer "+providerKey)
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	responseBytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return "", logger.Errorf("Unexpected status code: %s | %s", strconv.Itoa(response.StatusCode), string(responseBytes))
+	}
+
+	formattedResponse := utils.PrettyPrintJSON(responseBytes)
 	return formattedResponse, nil
 }
