e8d3583a01756d961d5065c1aec47b80dea7f05a
emancipat3r <23742165+emancipat3r@users.noreply.github.com>
2025-08-14 23:07:12 -0400

    Added check for DO account balance before running further tasks.

--- providers/digitalocean.go
+++ providers/digitalocean.go
@@ -1,6 +1,11 @@
 package providers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
 	"github.com/emancipat3r/vps3/logger"
 	"github.com/emancipat3r/vps3/utils"
 )
@@ -13,4 +18,56 @@ func GetDOAPIKey(configFile string, provider string) string {
 		return ""
 	}
 	return providerKey
+}
+
+type DOAccount struct {
+	Balance string `json:"month_to_date_balance"`
+}
+
+func GetDOBalance(providerKey string) (string, error) {
+	if providerKey == "" {
+		return "", logger.Errorf("Provider key is empty")
+	}
+
+	req, err := http.NewRequest("GET", "https://api.digitalocean.com/v2/customers/my/balance", nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Authorization", "Bearer "+providerKey)
+	req.Header.Set("Accept", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, _ := io.ReadAll(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		var errResp struct {
+			Errors []struct {
+				Reason string `json:"reason"`
+			} `json:"errors"`
+		}
+
+		if err := json.Unmarshal(bodyBytes, &errResp); err == nil && len(errResp.Errors) > 0 {
+			reason := errResp.Errors[0].Reason
+			if reason == "Invalid Token" {
+				return "", logger.Errorf("Your DigitalOcean key is invalid or expired. Check it in the config.")
+			}
+
+			return "", logger.Errorf("DigitalOcean API error: %s", reason)
+		}
+	}
+
+	var account DOAccount
+	err = json.Unmarshal(bodyBytes, &account)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%s", account.Balance), nil
+
 }
