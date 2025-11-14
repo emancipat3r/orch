f588cd9cb0eb760d8d35d4b04fac11685ac1e63f
emancipat3r <23742165+emancipat3r@users.noreply.github.com>
2025-11-13 22:41:51 -0500

    Finished logic to fully setup wg connection in new netns fixing dns resolution issues, working through teardown of netns to go with vps

---
+++ utils/instances.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/BurntSushi/toml"
+)
+
+// We only really care about vps_name for this path.
+type InstanceRecord struct {
+	VPSName string `toml:"vps_name"`
+}
+
+type InstanceDB map[string]InstanceRecord
+
+func LoadInstances(path string) (InstanceDB, error) {
+	db := InstanceDB{}
+	if _, err := toml.DecodeFile(path, &db); err != nil {
+		return nil, fmt.Errorf("decode instances.toml: %w", err)
+	}
+	return db, nil
+}
+
+// instanceID is the provider ID, e.g., "530066077"
+func GetVPSNameForInstance(path, instanceID string) (string, error) {
+	db, err := LoadInstances(path)
+	if err != nil {
+		return "", fmt.Errorf("load instances from %s: %w", path, err)
+	}
+
+	rec, ok := db[instanceID]
+	if !ok {
+		return "", fmt.Errorf("instance %s not found in %s", instanceID, path)
+	}
+	if rec.VPSName == "" {
+		return "", fmt.Errorf("instance %s has empty vps_name in %s", instanceID, path)
+	}
+
+	return rec.VPSName, nil
+}
